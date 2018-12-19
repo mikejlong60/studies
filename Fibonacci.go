5a20c913ecc9794a66518ac1fb5d14f827638c2a
michael.long <michael.long@deciphernow.com>
2018-12-19 16:24:00 -0500

    More work on Methos

--- Fibonacci.go
+++ Fibonacci.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-//This is bad because it uses mutation which means lots of good things can't happen.  Can't distribute, can't do in parallel, can't compose, etc.  We should not be writing programs in this archaic language.
+//This is bad because it uses mutation which means lots of good things can't happen.  Can't distribute, can't do in parallel, can't compose, etc.  We should not be writing programs in this style.
 func fibonacci() func() int {
 
 	var fst = 0
