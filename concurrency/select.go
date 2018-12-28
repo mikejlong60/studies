0ede8b295176177bc955fbe1eaec0eda380e4d46
michael.long <michael.long@deciphernow.com>
2018-12-28 18:14:52 -0500

    Some exercises around channels

--- concurrency/select.go
+++ concurrency/select.go
@@ -18,11 +18,11 @@ func fibonacci(c, quit chan int) {
 func main() {
 	c := make(chan int)
 	quit := make(chan int)
-	//	go func() {
-	//		for i := 0; i < 10; i++ {
-	//			fmt.Println(<-c)
-	//		}
-	//		quit <- 0
-	//	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
 	fibonacci(c, quit)
 }
